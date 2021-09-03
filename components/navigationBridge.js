8b33b0111bd3bb636882f5deb68256f4dbe25494
Wstld <52714297+Wstld@users.noreply.github.com>
2021-09-03 12:01:07 +0200

    logout btn fixed

--- components/navigationBridge.js
+++ components/navigationBridge.js
@@ -1,12 +1,12 @@
 
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { loginSlice,fbSignOut } from '../redux/features/loginSlice';
+import { loginSlice, fbSignOut } from '../redux/features/loginSlice';
 
 import HomeScreen from '../screens/home_screen';
 import FriendsScreen from '../screens/friends_screen';
@@ -17,20 +17,22 @@ import auth from '@react-native-firebase/auth'
 import store from '../redux/store';
 import VoteDetails from '../screens/voteDetails_screen';
 
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
 const NavigationBridge = () => {
     //navigation bridge to get access to global states.
 
     //handel auth change event.
-    function authChange(user){
-        
-        if(!user || user === undefined){
+    function authChange(user) {
+
+        if (!user || user === undefined) {
             dispatch(loginSlice.actions.reSetUser());
             dispatch(loginSlice.actions.setActiveUser(false));
-        }else{
-            if(user != store.getState().login.user){
+        } else {
+            if (user != store.getState().login.user) {
                 dispatch(loginSlice.actions.setUser(user));
-            } 
-            
+            }
+
             dispatch(loginSlice.actions.setActiveUser(true));
         }
     }
@@ -40,7 +42,7 @@ const NavigationBridge = () => {
         const authSubscribe = auth().onAuthStateChanged((user) => authChange(user));
         //callback to remove listner on unmount
         return authSubscribe;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
 
@@ -54,7 +56,7 @@ const NavigationBridge = () => {
 
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRouteName="Home" screenOptions={{
+            <Tab.Navigator initialRouteName="Home" screenOptions={({ route }) => ({
                 headerStyle: {
                     backgroundColor: 'grey',
                     elevation: 3,
@@ -63,26 +65,56 @@ const NavigationBridge = () => {
                 headerTitleStyle: {
                     fontWeight: 'bold',
                 },
-                headerRight: () => (
-                    <Button
-                        onPress={() => dispatch(fbSignOut()) }
-                        title="logout"
-                        color="red"
-                    />
-                ),
-            }}>
+                tabBarIcon: () => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = 'home'
+                    }
+                    if (route.name === 'Friends') {
+                        iconName = 'people'
+                    }
+                    if (route.name === 'AddQuestion') {
+                        iconName = 'help'
+                    }
+                    return <Ionicons name={iconName} size={24} />
+                },
+            })}>
                 {activeUser ? (
-                <Tab.Group>
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Friends" component={FriendsScreen} />
-                <Tab.Screen name="AddQuestion" component={AddQuestionScreen} />
-                </Tab.Group>
+                    <Tab.Group>
+                        <Tab.Screen name="Home" component={Home} options={({ navigation, route }) => ({
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => dispatch(fbSignOut())}
+                                    title="logout"
+                                    color="red"
+                                />
+                            ),
+                        })} />
+                        <Tab.Screen name="Friends" component={FriendsScreen} options={({ navigation, route }) => ({
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => dispatch(fbSignOut())}
+                                    title="logout"
+                                    color="red"
+                                />
+                            ),
+                        })} />
+                        <Tab.Screen name="AddQuestion" component={AddQuestionScreen} options={({ navigation, route }) => ({
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => dispatch(fbSignOut())}
+                                    title="logout"
+                                    color="red"
+                                />
+                            ),
+                        })} />
+                    </Tab.Group>
                 ) :
-                (
-                    <>
-                    <Tab.Screen name="Login" component={LoginScreen} />
-                    </>
-                )
+                    (
+                        <>
+                            <Tab.Screen name="Login" component={LoginScreen} />
+                        </>
+                    )
                 }
             </Tab.Navigator>
         </NavigationContainer>
@@ -93,10 +125,10 @@ const NavigationBridge = () => {
 const Home = () => {
     const Stack = createNativeStackNavigator();
     return (
-    <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name = "Landing" component = { HomeScreen }/>
-        <Stack.Screen name = "Details" component = { VoteDetails } />
-    </Stack.Navigator>
+        <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={HomeScreen} />
+            <Stack.Screen name="Details" component={VoteDetails} />
+        </Stack.Navigator>
     );
 }
 
